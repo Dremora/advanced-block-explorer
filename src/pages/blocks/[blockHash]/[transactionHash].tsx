b391d3eb6bb0a902273e088a9d903ba83ae2ec8f
Kirill Korolyov <kirill.korolyov@gmail.com>
2021-02-12 01:12:23 +0000

    Tick for effective

--- src/pages/blocks/[blockHash]/[transactionHash].tsx
+++ src/pages/blocks/[blockHash]/[transactionHash].tsx
@@ -257,7 +257,7 @@ export default function Transaction({
             <KeyValue>
               <Key>Effective: </Key>
               <Value>
-                {selectedTransactionItem.message.effective ? "True" : "False"}
+                {selectedTransactionItem.message.effective ? "✅" : "❌"}
               </Value>
             </KeyValue>
 
